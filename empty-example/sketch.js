ecad585ed6a1a3b1a43f90e7cd15f7b1ac772336
Nick Joaquin <njj2113@tc.columbia.edu>
2019-08-18 09:09:10 -0400

    setting up object list

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,31 @@
+var circleY;
+var circleX = 50;
+var col = 0;
+let a = 5;
+
 function setup() {
   // put setup code here
+  createCanvas(800,600);
+  print("Presto");
+  circleY = 50;
+  background(220,0,100);
 }
 
 function draw() {
   // put drawing code here
+  col = map(mouseX,0,800,255,0);
+  background(col, col, 255-col);
+  circleY += 10;
+  ellipse(mouseX,circleY,50,50);
+  rect(100,100,500,50);
+
+  if (circleY > 600)
+  {
+    fill(random(0,255),random(0,255),random(0,255));
+    circleY = 0;
+  }
 }
+
+//function mousePressed() {
+//  background(220,0,100)
+//}
--- proe/sketch.js
+++ proe/sketch.js
@@ -1,31 +1,33 @@
-var circleY;
-var circleX = 50;
-var col = 0;
-let a = 5;
+let assignment;
+var assignments = [];
 
 function setup() {
-  // put setup code here
   createCanvas(800,600);
-  print("Presto");
-  circleY = 50;
-  background(220,0,100);
+  assignments.push(new Assignment);
+  print(assignments.length);
 }
 
 function draw() {
-  // put drawing code here
-  col = map(mouseX,0,800,255,0);
-  background(col, col, 255-col);
-  circleY += 10;
-  ellipse(mouseX,circleY,50,50);
-  rect(100,100,500,50);
-
-  if (circleY > 600)
-  {
-    fill(random(0,255),random(0,255),random(0,255));
-    circleY = 0;
-  }
+  background(200,100,240);
+  assignment.move();
+  assignment.show();
 }
 
-//function mousePressed() {
-//  background(220,0,100)
-//}
+class Assignment {
+  constructor() {
+    this.x = 200;
+    this.y = 200;
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(assignment.x,assignment.y,50,50);
+  }
+
+  move() {
+    assignment.x += random(-5,5);
+    assignment.y += random(-5,5);
+  }
+}
