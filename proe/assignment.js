77fdcba6a9d45de2f0bf9212100ec64f9673028e
Nick Joaquin <njj2113@tc.columbia.edu>
2019-08-18 10:49:26 -0400

    amid 7.5

--- proe/assignment.js
+++ proe/assignment.js
@@ -1,15 +1,27 @@
 class Assignment {
-  constructor(x, y, z) {
+  constructor(x, y, r, red, green, blue) {
     this.x = x;
     this.y = y;
-    this.z = z;
+    this.r = r;
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+  }
+
+  clicked(x, y) {
+    let d = dist(x, y, this.x, this.y);
+    if (d < this.r) {
+      this.red = random(0,255);
+      this.green = random(0,255);
+      this.blue = random(0,255);
+    }
   }
 
   show() {
     stroke(255);
     strokeWeight(4);
-    noFill();
-    ellipse(this.x,this.y,50,50);
+    fill(this.red, this.green, this.blue)
+    ellipse(this.x,this.y,this.r);
   }
 
   move() {
--- proe/sketch.js
+++ proe/sketch.js
@@ -1,15 +1,34 @@
-let assignment;
+let assignments = [];
+//let assignment;
 
 function setup() {
   createCanvas(800,600);
-  assignment = new Assignment(200,200,200);
-  assignment2 = new Assignment(200,200,200);
+  assignments[0] = new Assignment(200,200,200,0,0,0);
+  assignments[1] = new Assignment(200,200,200,255,255,255);
+}
+
+//mouseDragged would be even cooler
+function mousePressed() {
+  //let r = random(10,50);
+  //let a = new Assignment(mouseX,mouseY,r,0,0,0);
+  //assignments.push(a);
+
+  for (let i = assignments.length-1; i >= 0; i--) {
+    assignments[i].clicked(mouseX, mouseY);
+  }
 }
 
 function draw() {
-  background(200,100,240);
+background(200,100,240);
+//for (let i = 0; i < assignments.length; i++)
+//{
+//  assignments[i].move();
+//  assignments[i].show();
+//}
+
+for (let assignment of assignments) {
   assignment.move();
   assignment.show();
-  assignment2.move();
-  assignment2.show();
+}
+
 }
